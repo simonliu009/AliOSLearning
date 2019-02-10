f8122ae40af9e72767d843b3c463bfd8154496b2
Simon Liu <sliu009@gmail.com>
2019-02-10 21:30:03 +0800

    1. Clean up useless code.

--- GPIO_LED/GPIO_LED.c
+++ GPIO_LED/GPIO_LED.c
@@ -13,20 +13,16 @@
  * This test code shows how to configure gpio and how to use gpio interrupt.
  *
  * GPIO status:
- * GPIO18: output
  * GPIO4: output
- * GPIO5:  input, pulled up, interrupt from rising edge and falling edge
+ * GPIO5: output
+ * GPIO14:  input, pulled up, interrupt from falling edge
  *
  * Test:
- * Connect GPIO18 with LED
- * Connect GPIO4 with GPIO5
- * Generate pulses on GPIO4, that triggers interrupt on GPIO5 to blink the led.
+ * LED blink on GPIO4
+ * LED toggled on GPIO5 by push button on GPIO14
  *
  */
 
-// #define GPIO_LED_IO     18
-// #define GPIO_TRIGGER_IO 4
-// #define GPIO_INPUT_IO   5
 
 #define GPIO_LED1   4
 #define GPIO_LED2   5
@@ -38,54 +34,20 @@ static void toggleLed2(void);
 
 static void gpio_isr_handler(void *arg);
 
-// static void led_toggle_timer_cb(void);
-// aos_timer_t led_toggle_timer;
-
-
-// static void led_toggle_timer_cb(void)
-// {
-//     hal_gpio_output_toggle(&led2);
-//     LOG("LOG ouput: Toggle LED2 \r\n");
-// }
-// static void led_toggle(void)
-// {
-//     // aos_timer_new_ext(&led_toggle_timer, led_toggle_timer_cb,NULL,1000,1,0);
-//     aos_timer_new(&led_toggle_timer, led_toggle_timer_cb, NULL, 1000, 1);
-//     aos_timer_start(&led_toggle_timer);
-
-// }
 
 gpio_dev_t led1;
 gpio_dev_t led2;
 gpio_dev_t input;
 
-uint32_t lastPushed;
 
 
 static void gpio_isr_handler(void* arg)
 {
-    // uint32_t gpio_num = (uint32_t) arg;
-    // uint32_t value = 0;
-    
-    // long long lastPushedMs = 0;
-    // long long currentMs;
-    // currentMs = aos_now_ms();
-    // uint32_t lastPushedMs = 0;
-    uint32_t currentMs;
-    currentMs = (uint32_t)aos_now_ms();
 
-    if ((currentMs - lastPushed) > 50)
-    {
     hal_gpio_disable_irq(&input);
     hal_gpio_output_toggle(&led1);
     hal_gpio_clear_irq(&input);
-    lastPushed = currentMs;
-    }
-
-    // hal_gpio_input_get(&input, &value);
-    // LOG("GPIO[%d] intr, val: %d\n", gpio_num, value);
-
-
+   
 } 
 
 
@@ -133,33 +95,14 @@ static void toggleLed2(void)
     hal_gpio_output_toggle(&led2);
     aos_post_delayed_action(500, toggleLed2, NULL);
     LOG("LOG ouput: Toggle LED2 \r\n");
-    printf("Printf ouput: Toggle LED2 \r\n");
+
 }
 
 int application_start(int argc, char *argv[])
 {
-    // uart_config_t uartConfig;
-    // uart_dev_t uart;
-    // uartConfig.baud_rate = 115200; 
-    // uart_dev_t uart;
-    // uart.port = 2; //for esp8266 , when the port set is 1 ,then the uart1 ande uart2 is the same baud_rate , but the uart1 is for log 
-    // uart.config = uartConfig;
 
-   uart_dev_t uart_2;
-   uart_2.port                = 2;
-   uart_2.config.baud_rate    = 115200;
-   uart_2.config.data_width   = DATA_WIDTH_8BIT;
-   uart_2.config.parity       = NO_PARITY;
-   uart_2.config.stop_bits    = STOP_BITS_1;
-   uart_2.config.flow_control = FLOW_CONTROL_DISABLED;
-   hal_uart_init(&uart_2);
-
-
-    hal_uart_init(&uart_2);
-    lastPushed = (uint32_t)aos_now_ms();
     gpioInit();
     aos_post_delayed_action(1000, toggleLed2, NULL);
-    // led_toggle();
     aos_loop_run();
 
     return 0;
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AliOSLearning
 [SimonLiu的ESP8266与AliOS Things 学习系列](https://blog.csdn.net/toopoo/article/details/86920755)源码
+目录：
+[1. SimonLiu的ESP8266与AliOS Things 学习系列之一：编译环境搭建（Windows10)](https://blog.csdn.net/toopoo/article/details/86773730)
+[2. SimonLiu的ESP8266与AliOS Things 学习系列之二：新建项目](https://blog.csdn.net/toopoo/article/details/86777012)
